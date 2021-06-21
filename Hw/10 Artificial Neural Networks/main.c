99b677df1728fc4ab648ca21dd8a926c63bba9ee
TheOakStaff <crelleovergaard@hotmail.com>
2021-06-21 14:54:32 +0200

    Updated h and c files to specifications

---
+++ Hw/10 Artificial Neural Networks/main.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include "math.h"
+#include "stdlib.h"
+#include "float.h"
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+#include "My_vec_calc.h"
+#include <assert.h>
