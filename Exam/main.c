6707592b240b5a2b80760b0b4faab378a2a170ce
TheOakStaff <crelleovergaard@hotmail.com>
2021-06-26 14:59:32 +0200

    Exam Init

--- Exam/main.c
+++ Exam/main.c
@@ -4,4 +4,13 @@
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
+
+// HOMEMADE INCLUSIONS //
+
 #include "My_vec_calc.h"
+#include "SVD.h"
+
+int main() {
+	printf("Make test\n");
+	return 0;
+}
